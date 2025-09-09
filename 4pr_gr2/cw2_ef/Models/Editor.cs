522a91a88d2280daf2379a789330d38e3a18abb8
tiszczesz <tiszczesz@users.noreply.github.com>
2025-09-09 09:32:44 +0200

    kk

---
+++ 4pr_gr2/cw2_ef/Models/Editor.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace cw2_ef.Models;
+
+public class Editor
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+}
