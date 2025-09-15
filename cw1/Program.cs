64a3ce0a1a20809dd2274cc1c22b2dabca079fdb
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-09-15 16:34:55 +0200

    kk

---
+++ cw1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Podaj tekst: ");
+// Console.WriteLine("sadf asa dsa da das");
+string? answer = Console.ReadLine();
+Console.WriteLine("Twoja odpowiedz: "+answer);
+Console.WriteLine($"Twoja odpowiedz: {answer}");
