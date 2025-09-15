be83ede9cb7c15755e6345d3361e5e0ce716315a
pszczeszek <you@example.com>
2025-09-15 15:29:19 +0000

    kk

--- 3pr_gr2/cw1/Program.cs
+++ 3pr_gr2/cw1/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿//ex1();
+ex2();
+void ex1()
+{
+    const string FirstName = "John";
+    const string LastName = "Doe";
+    Console.WriteLine($"Hello, {FirstName} {LastName}!");
+}
+void ex2()
+{
+    Console.Write("Podaj swoje imie: ");
+    string? firstName = Console.ReadLine()?.Trim();
+    Console.Write("Podaj swoje nazwisko: ");
+    string? lastName = Console.ReadLine()?.Trim();
+    if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+    {
+        Console.WriteLine("Brak danych");
+        
+    }
+    else
+    {
+        Console.WriteLine(
+            $"Witaj, {firstName} {lastName}" +
+            $" dzisiaj mamy: {DateTime.Now.ToShortDateString()}!"
+            );
+    }
+}
+void ex3()
+{
+    //liczby
+}
