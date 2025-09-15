a2b8d172af3ca02d9ccbacb90ed386de02255d58
pszczeszek <you@example.com>
2025-09-15 16:17:50 +0000

    kk

--- 3pr_gr1/cw1/Program.cs
+++ 3pr_gr1/cw1/Program.cs
@@ -1,7 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("Podaj tekst: ");
-// Console.WriteLine("sadf asa dsa da das");
-string? answer = Console.ReadLine();
-Console.WriteLine("Twoja odpowiedz: "+answer);
-Console.WriteLine($"Twoja odpowiedz: {answer}");
+﻿ex1();
+
+void ex1()
+{
+    // See https://aka.ms/new-console-template for more information
+    Console.WriteLine("Hello, World!");
+    Console.Write("Podaj tekst: ");
+    // Console.WriteLine("sadf asa dsa da das");
+    string? answer = Console.ReadLine();
+    Console.WriteLine("Twoja odpowiedz: " + answer);
+    Console.WriteLine($"Twoja odpowiedz: {answer}");
+}
