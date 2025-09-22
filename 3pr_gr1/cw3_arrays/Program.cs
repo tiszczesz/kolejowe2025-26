3473c19167870382756dc820aa9aa8e473bbaf92
pszczeszek <you@example.com>
2025-09-22 17:23:54 +0000

    kk

---
+++ 3pr_gr1/cw3_arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿ex1();
+void ex1()
+{
+    //tablce jednowymiarowe
+    //typ[] nazwa = new typ[rozmiar];
+    //tablica z inicjalizacja
+    string[] colors = { "czerwony", "zielony", "niebieski" };
+    foreach (string color in colors)
+    {
+        Console.Write(color + " ");
+    }
+    Console.WriteLine();
+    int[] numbers = new int[10]; //tablica 10 elementowa 0000000000
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Random.Shared.Next(1, 101);//i * 10;
+    }
+    //numbers[10] = 44;
+    foreach (int number in numbers)
+    {
+        Console.Write(number + " ");
+    }
+
+    Console.WriteLine();
+}
