a7a4860202c0173ad9eba5e677beed2ff2bb982d
pszczeszek <you@example.com>
2025-09-22 16:39:24 +0000

    kk

---
+++ 3pr_gr1/cw2/Program.cs
@@ -0,0 +1,42 @@
+﻿//ex1();
+void ex1()
+{
+    //petla for
+    for (int i = 0; i < 10; i++)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+ex2();
+void ex2()
+{
+    //petla while
+    //oblicza sume liczb losowych az przekroczy 100
+    int sum = 0;
+    int counter = 0;
+    int minValue = int.MaxValue;
+    int maxValue = int.MinValue;
+    //Random rand = new Random();
+    while (sum < 100)
+    {
+        int randNum = Random.Shared.Next(1, 21);
+        if (randNum < minValue) minValue = randNum;
+        if (randNum > maxValue) maxValue = randNum;
+        counter++;
+        sum += randNum;
+        Console.Write(randNum + " ");
+    }
+    if (counter > 0)
+    {
+        Console.WriteLine("Suma: " + sum);
+        Console.WriteLine("Srednia: " + ((double)sum / counter));
+        Console.WriteLine("min: " + minValue);
+        Console.WriteLine("max: " + maxValue);
+    }
+    else
+    {
+        Console.WriteLine("Nie wylosowano zadnej liczby");
+    }
+
+}
