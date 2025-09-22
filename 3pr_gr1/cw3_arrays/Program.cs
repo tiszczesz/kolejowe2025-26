daf6478bfab90ad8dcef9b8fecf78e98d8117651
pszczeszek <you@example.com>
2025-09-22 17:31:54 +0000

    kk

--- 3pr_gr1/cw3_arrays/Program.cs
+++ 3pr_gr1/cw3_arrays/Program.cs
@@ -1,4 +1,4 @@
-﻿ex1();
+﻿//ex1();
 void ex1()
 {
     //tablce jednowymiarowe
@@ -23,3 +23,27 @@ void ex1()
 
     Console.WriteLine();
 }
+ex2();
+void ex2()
+{
+    //tablice wielowymiarowe
+    //tablice 2D
+    int[,] table = new int[3, 4]; //3 wiersze 4 kolumny
+    int rows = table.GetLength(0); //3
+    int cols = table.GetLength(1); //4
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            table[r, c] = r * c;
+        }
+    }
+    //wyswietlanie
+    for (int r = 0; r < rows; r++){
+        for (int c = 0; c < cols; c++)
+        {
+            Console.Write(table[r, c]+"\t");
+        }
+        Console.WriteLine();
+    }
+ }
