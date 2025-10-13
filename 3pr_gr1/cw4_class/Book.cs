f3cd76df0ebcee6ed000792e79579ae3f6086151
pszczeszek <you@example.com>
2025-10-13 17:06:09 +0000

    kk

--- 3pr_gr1/cw4_class/Book.cs
+++ 3pr_gr1/cw4_class/Book.cs
@@ -4,9 +4,24 @@ public class Book
 {
     private string title;
     private string author;
+    private bool isDigital;
     private int year;
     public override string ToString()
     {
-        return $"Title: {title}, Author: {author}, Year: {year}";
+        return $"Title: {title}, Author: {author}, Year: {year} (Digital: {isDigital})";
+    }
+    public Book()
+    {
+        title = "Unknown Title";
+        author = "Unknown Author";
+        isDigital = false;
+        year = DateTime.Now.Year;
+    }
+    public Book(string title, string author, bool isDigital, int year)
+    {
+        this.title = title;
+        this.author = author;
+        this.isDigital = isDigital;
+        this.year = year;
     }
 }
