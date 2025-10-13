23e287e49bf7773e3129cc01fc66fb0e795ee1e4
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-10-13 15:55:55 +0200

    kk

--- 4pr_gr2/cw5_api/Models/script.sql
+++ 4pr_gr2/cw5_api/Models/script.sql
@@ -5,3 +5,8 @@ CREATE TABLE Students (
     LastName TEXT NOT NULL,
     BirthDate DATE    
 );
+INSERT INTO Students (FirstName, LastName, BirthDate) VALUES
+('Anna', 'Kowalska', '2000-05-12'),
+('Jan', 'Nowak', '1999-11-23'),
+('Maria', 'Wiśniewska', '2001-03-17'),
+('Piotr', 'Zieliński', '2000-08-30');
