152ce4b5ee6cb430ff7b51c5adde0988cd46088f
pszczeszek <you@example.com>
2025-10-13 15:58:42 +0000

    kk

--- 3pr_gr2/cw4_class/Program.cs
+++ 3pr_gr2/cw4_class/Program.cs
@@ -1,5 +1,8 @@
 ﻿using cw4_class;
 
 Student s1 = new Student();
+Student s2 = new Student("Jane", "Smith");
+s1.FirstName = "Alice";
+s1.LastName = "Johnson";
 Console.WriteLine(s1.ToString());
-Console.WriteLine(s1);
+Console.WriteLine(s2);
--- 3pr_gr2/cw4_class/Student.cs
+++ 3pr_gr2/cw4_class/Student.cs
@@ -1,3 +1,31 @@
 namespace cw4_class;
 
-public class Student{}
+public class Student{
+    private string firstName;
+    private string lastName;
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}";
+    }
+    public Student()
+    {
+        firstName = "John";
+        lastName = "Doe";
+    }
+    public Student(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    //properties
+    public string FirstName
+    {
+        get { return firstName.ToUpper(); }
+        set { firstName = value.Trim(); }
+    }
+    public string LastName
+    {
+        get { return lastName.ToUpper(); }
+        set { lastName = value.Trim(); }
+    }
+}
