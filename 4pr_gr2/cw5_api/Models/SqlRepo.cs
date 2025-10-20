413dbc860a79716fc0181097131e4e752b6d1625
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-10-20 16:06:58 +0200

    kk

--- 4pr_gr2/cw5_api/Models/SqlRepo.cs
+++ 4pr_gr2/cw5_api/Models/SqlRepo.cs
@@ -12,12 +12,27 @@ public class SqlRepo : IRepo
     }
     public void AddStudent(Student student)
     {
-        throw new NotImplementedException();
+        using var con = new SqliteConnection(_connectionString);
+        using var cmd = con.CreateCommand();
+        cmd.CommandText = "INSERT INTO Students (FirstName, LastName, BirthDate) "
+                      + " VALUES (@firstName, @lastName, @birthDate)";
+        cmd.Parameters.AddWithValue("@firstName", student.FirstName);
+        cmd.Parameters.AddWithValue("@lastName", student.LastName);
+        cmd.Parameters.AddWithValue("@birthDate", student.BirthDate);
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
     }
 
     public void DeleteStudent(int id)
     {
-        throw new NotImplementedException();
+        using var con = new SqliteConnection(_connectionString);
+        using var cmd = con.CreateCommand();
+        cmd.CommandText = "DELETE FROM Students WHERE Id = @id";
+        cmd.Parameters.AddWithValue("@id", id);
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
     }
 
     public List<Student> GetAll()
@@ -45,11 +60,42 @@ public class SqlRepo : IRepo
 
     public Student? GetStudent(int id)
     {
-        throw new NotImplementedException();
+        using var con = new SqliteConnection(_connectionString);
+        using var cmd = con.CreateCommand();
+        cmd.CommandText = "SELECT Id, FirstName, LastName, BirthDate "
+           + "FROM Students WHERE Id = @id";
+        cmd.Parameters.AddWithValue("@id", id);
+        con.Open();
+        using var reader = cmd.ExecuteReader();
+        if (reader.HasRows == false)
+        {
+            return null;
+        }
+        reader.Read();
+        var student = new Student
+        {
+            Id = reader.GetInt32(0),
+            FirstName = reader.GetString(1),
+            LastName = reader.GetString(2),
+            BirthDate = reader.GetDateTime(3)
+        };
+        con.Close();
+        return student;
     }
 
     public void UpdateStudent(Student student)
     {
-        throw new NotImplementedException();
+        using var con = new SqliteConnection(_connectionString);
+        using var cmd = con.CreateCommand();
+        cmd.CommandText = "UPDATE Students SET FirstName = @firstName, "
+                      + " LastName = @lastName, BirthDate = @birthDate "
+                      + " WHERE Id = @id";
+        cmd.Parameters.AddWithValue("@firstName", student.FirstName);
+        cmd.Parameters.AddWithValue("@lastName", student.LastName);
+        cmd.Parameters.AddWithValue("@birthDate", student.BirthDate);
+        cmd.Parameters.AddWithValue("@id", student.Id);
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
     }
 }
--- 4pr_gr2/cw5_api/Program.cs
+++ 4pr_gr2/cw5_api/Program.cs
@@ -3,12 +3,58 @@ using cw5_api.Models;
 var builder = WebApplication.CreateBuilder(args);
 //dodaje do kontenera DI FakeRepo jako implementacje IRepo
 builder.Services.AddScoped<IRepo, SqlRepo>();
+//dodanie CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+
 var app = builder.Build();
 //pobieram z kontenera DI implementacje IRepo
 //var repo = app.Services.GetRequiredService<IRepo>();
-
+app.UseCors("AllowAll");
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/students", (IRepo repo) => repo.GetAll());
+app.MapGet("/students/{id}", (IRepo repo, int id) =>
+{
+    var student = repo.GetStudent(id);
+    if (student == null)
+    {
+        return Results.NotFound("Student not found");
+    }
+    return Results.Ok(student);
+});
+app.MapPost("/students", (IRepo repo, Student student) =>
+    {
+        repo.AddStudent(student);
+        return Results.Ok("Student added");
+    });
+
+app.MapDelete("/students/{id}", (IRepo repo, int id) =>
+{
+    repo.DeleteStudent(id);
+    return Results.Ok("Student deleted");
+});
+
+app.MapPut("/students/{id}", (IRepo repo,int? id,Student student) =>
+{
+    Student? studentToUpdate = repo.GetStudent(id ?? 0);
+    if (studentToUpdate == null)
+    {
+        return Results.NotFound("Student not found");
+    }
+    student.Id = id ?? 0;
+    repo.UpdateStudent(student);
+    return Results.Ok("Student updated");
+});
+
 // app.MapGet("/students/{id}", (int id) =>
 // {
 //     var student = repo.GetStudent(id);
