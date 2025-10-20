6ca4ce9dea08932b0d7a3ef31b150c1b7110cc5d
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-10-20 17:12:31 +0200

    kk

--- 3pr_gr1/cw4_class/Models/BooksFileRepo.cs
+++ 3pr_gr1/cw4_class/Models/BooksFileRepo.cs
@@ -5,5 +5,20 @@ namespace cw4_class.Models;
 public class BooksFileRepo
 {
     public List<Book> Books { get; set; }
-    
+    private Book? ParseLine(string line)
+    {
+        var parts = line.Split(';');
+        if (parts.Length != 5)
+        {
+            return null;
+        }
+        return new Book
+        {
+            Id = int.Parse(parts[0]),
+            Title = parts[1],
+            Author = parts[2],
+            Year = int.Parse(parts[3]),
+            IsDigital = bool.Parse(parts[4])
+        };
+    }
 }
