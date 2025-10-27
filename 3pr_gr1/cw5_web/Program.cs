7295b4862166ef0eb8d1d18b60fe6d787ba77939
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-10-27 17:11:04 +0100

    kk

--- 3pr_gr1/cw5_web/Program.cs
+++ 3pr_gr1/cw5_web/Program.cs
@@ -1,9 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 //dodawanie nowych usług do kontenera
+//dodawanie  Razor Pages itp.
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
+//konfigurowanie potoku przetwarzania żądań HTTP
+app.UseStaticFiles();
 var info = "Ala ma kota";
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.MapRazorPages();
 app.MapGet("/first", () => "Hello First!");
 app.MapGet("/info", () => info.ToUpper());
 app.MapGet("/data", () => DateTime.Now.ToShortDateString());
