50d71e4ddff22b4db0bbf77fedd9c6eb0b2da51d
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-12-01 19:38:46 +0100

    kk

--- 3pr_gr2/cw6_forms/Pages/Form1.cshtml.cs
+++ 3pr_gr2/cw6_forms/Pages/Form1.cshtml.cs
@@ -1,3 +1,4 @@
+using cw6_forms.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,19 @@ namespace cw6_forms.Pages
 {
     public class Form1Model : PageModel
     {
+        public Person  MyPerson{get;set;} = new Person();
         public void OnGet()
         {
+            if(Request.Query.ContainsKey("firstname"))
+            {
+                ViewData["Message"] = "Dane zostały przesłane metodą GET";
+                MyPerson.FirstName = Request.Query["firstname"];
+                MyPerson.LastName = Request.Query["lastname"];
+                MyPerson.Age = Convert.ToInt32(Request.Query["age"]);                
+            }
+            
         }
     }
+
+   
 }
