2937b17d32fc5b636aab0afebef9ed52b7fffbe1
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-12-01 17:28:48 +0100

    kk

---
+++ 3pr_gr2/cw6_forms/Pages/Form1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cw6_forms.Pages
+{
+    public class Form1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
