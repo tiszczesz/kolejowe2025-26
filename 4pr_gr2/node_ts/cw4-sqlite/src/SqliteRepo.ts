bec27b1a930b3105fd8ccd79a7deee5fe01228d1
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-12-09 10:21:03 +0100

    kk

--- 4pr_gr2/node_ts/cw4-sqlite/src/SqliteRepo.ts
+++ 4pr_gr2/node_ts/cw4-sqlite/src/SqliteRepo.ts
@@ -6,8 +6,32 @@ import { fileURLToPath } from 'url';
 // console.log(import.meta.url);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+export type User =  {
+    id: number;
+    name: string;
+    email: string;
+    created_at: string; // DATETIME (ISO string) from SQLite
+}
+
 
 // console.log(__dirname);
 // console.log(__filename);
 const dbPath = path.join(__dirname,'..', 'appDB.db'); //sciezka do bazy danych
 console.log(dbPath);
+
+let db: Database | null = null; // Zmienna do przechowywania instancji bazy danych
+
+export const initDB = async (): Promise<Database> => {
+    if(db) return db; // jeśli baza już jest zainicjalizowana, zwróć ją
+    db = await open({
+        filename: dbPath,
+        driver: sqlite3.Database
+    });
+    return db;
+}
+ 
+export const getAllUsers = async (): Promise<User[]|undefined> => {
+    const database = await initDB();
+    const users: User[]|undefined = await database.get<User[]>('SELECT * FROM users');
+    return users;
+}
