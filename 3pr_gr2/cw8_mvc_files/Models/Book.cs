cdb3be7197c8101abed12492f71dcbf0de687448
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-12-15 17:28:15 +0100

    kk

---
+++ 3pr_gr2/cw8_mvc_files/Models/Book.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cw8_mvc_files.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int YearPublished { get; set; }
+}
---
+++ 3pr_gr2/cw8_mvc_files/Models/IBooksRepo.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cw8_mvc_files.Models;
+
+public interface IBooksRepo
+{
+    public List<Book> GetBooks();
+    public void AddBook(Book book);
+    public Book? GetBookById(int id);
+    public void UpdateBook(Book book);
+}
