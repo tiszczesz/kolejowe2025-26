d61e9d68feb327b6d8836479cd8f57f3f17dd23f
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-12-15 19:34:41 +0100

    kk

--- 3pr_gr1/cw7_mvc_file/Models/Movie.cs
+++ 3pr_gr1/cw7_mvc_file/Models/Movie.cs
@@ -1,11 +1,22 @@
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace cw7_mvc_file.Models;
 
 public class Movie
 {
     public int Id { get; set; }
+    
+    [DisplayName("Tytuł")]
+    [Required(ErrorMessage = "Pole Tytuł jest wymagane")]
     public string Title { get; set; }
+    
+     [DisplayName("Reżyser")]
+    [Required(ErrorMessage = "Pole Reżyser jest wymagane")]
     public string Director { get; set; }
-    public int Year { get; set; }
+    
+    [DisplayName("Rok produkcji")]
+    [Required(ErrorMessage = "Pole Rok produkcji jest wymagane")]
+    public int? Year { get; set; }
 }
