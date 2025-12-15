c63512077e63e6236a3b8ab98e983230ad1af14a
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2025-12-15 19:28:28 +0100

    kk

--- 3pr_gr1/cw7_mvc_file/Models/FileMoviesRepo.cs
+++ 3pr_gr1/cw7_mvc_file/Models/FileMoviesRepo.cs
@@ -6,15 +6,28 @@ namespace cw7_mvc_file.Models;
 public class FileMoviesRepo : IMoviesRepo
 {
     private readonly string _filePath = "movies.json";
-    private List<Movie> LoadFromFile()
+    private List<Movie> LoadFromFile() //deserializacja
     {
         string json = File.ReadAllText(_filePath);
         return JsonSerializer.Deserialize<List<Movie>>(json) 
                ?? new List<Movie>();
     }
+    private void SaveToFile(List<Movie> movies)//serializacja
+    {
+        string json = JsonSerializer.Serialize(movies);
+        File.WriteAllText(_filePath, json);
+    }
+    private int GetNextId(){ //autoinkrementacja Id
+        var movies = LoadFromFile();
+        if(movies.Count == 0) return 1;
+        return movies.Max(m => m.Id) + 1;
+    }
     public void AddMovie(Movie movie)
     {
-        throw new NotImplementedException();
+        movie.Id = GetNextId();
+        var movies = LoadFromFile();
+        movies.Add(movie);
+        SaveToFile(movies);
     }
 
     public void Delete(int id)
