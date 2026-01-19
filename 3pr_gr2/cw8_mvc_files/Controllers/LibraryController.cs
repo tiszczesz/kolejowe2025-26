2a013392c4b4523d69607e751e0a3faab7cb2453
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-01-19 17:48:59 +0100

    kkk

--- 3pr_gr2/cw8_mvc_files/Controllers/LibraryController.cs
+++ 3pr_gr2/cw8_mvc_files/Controllers/LibraryController.cs
@@ -28,8 +28,37 @@ namespace cw8_mvc_files.Controllers
         {
             //aby odebrać dane z formularza
             //dodać książkę do repozytorium
-            _booksRepo.AddBook(book);
+            if(ModelState.IsValid)
+            {
+               _booksRepo.AddBook(book);
+                return RedirectToAction("List");
+            }
+            return View(book);
+            
+        }
+        public IActionResult DeleteBook(int id)
+        {
+            _booksRepo.DeleteBook(id);
             return RedirectToAction("List");
+        }
+        public IActionResult UpdateBook(int id)
+        {
+            var book = _booksRepo.GetBookById(id);
+            if(book == null)
+            {
+                return RedirectToAction("List");
+            }
+            return View(book);
+        }
+        [HttpPost]
+        public IActionResult UpdateBook(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _booksRepo.UpdateBook(book);
+                return RedirectToAction("List");
+            }
+            return View(book);
         }
 
     }
