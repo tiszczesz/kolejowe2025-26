29c07d08374db564d50db2bc7d669a4dc10a0e22
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-02-16 15:37:38 +0100

    kk

--- 4pr_gr2/react/cw1-react/src/App.tsx
+++ 4pr_gr2/react/cw1-react/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Component1 from './components/Component1'
 
 function App() {
 
@@ -6,7 +7,9 @@ function App() {
   return (
     <>
       <h1>Hello from React</h1>
+      <p>Akapit a App.tsx</p>
       <hr />
+      <Component1 />
       <ul>
         <li>asdasd</li>
         <li>asdasd</li>
