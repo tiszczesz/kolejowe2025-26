c9a6e2e91386513f2a66025978a396e391dd8c67
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-02-16 15:14:16 +0100

    kkk

---
+++ 4pr_gr2/react/cw1-react/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello from React</h1>
+      <hr />
+      <ul>
+        <li>asdasd</li>
+        <li>asdasd</li>
+        <li>asdasd</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
