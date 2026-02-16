1cc7f4668118b28770ea9eb3a0e401c840ce83df
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-02-16 13:11:49 +0100

    kk

--- 4pr_gr1/nodde_typescript/cw1/dist/first.js
+++
@@ -1,5 +0,0 @@
-import { colors } from './my-module.js';
-for (const c of colors) {
-    console.log(`element tablicy: ${c}`);
-}
-//# sourceMappingURL=first.js.map
--- 4pr_gr1/nodde_typescript/cw1/dist/my-module.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const colors: string[];
-//# sourceMappingURL=my-module.d.ts.map
