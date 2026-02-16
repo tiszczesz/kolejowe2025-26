5000488ce192d21f728eddbad2f663057f982032
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-02-16 19:27:35 +0100

    kk

--- 3pr_gr1/cw7_mvc_file/Models/SqliteMoviesRepo.cs
+++ 3pr_gr1/cw7_mvc_file/Models/SqliteMoviesRepo.cs
@@ -26,7 +26,13 @@ public class SqliteMoviesRepo : IMoviesRepo
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        using var connection = new SqliteConnection(_connectionString);
+        using var command = connection.CreateCommand();
+        command.CommandText = @"DELETE FROM Movies WHERE id = @id";
+        command.Parameters.AddWithValue("@id", id);
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
     }
 
     public List<Movie> GetAll()
@@ -55,11 +61,42 @@ public class SqliteMoviesRepo : IMoviesRepo
 
     public Movie? GetById(int id)
     {
-        throw new NotImplementedException();
+        //obiekt connection do bazy danych, korzystamy z using, zeby po zakonczeniu uzywania polaczenia bylo ono zamkniete
+        using var connection = new SqliteConnection(_connectionString);
+        //tworzymy obiekt command, ktory pozwala nam wykonac zapytanie do bazy danych
+        using var command = connection.CreateCommand();
+        //zapytanie SQL, ktore chcemy wykonac
+        command.CommandText = "SELECT id, title, director,year FROM Movies WHERE id = @id";
+        command.Parameters.AddWithValue("@id", id);
+        //otwieramy polaczenie z baza danych
+        connection.Open();
+        using var reader = command.ExecuteReader();
+        if(!reader.HasRows) return null; //nie znaleziono filmu o podanym id
+        Movie? movie = null;
+        if (reader.Read())
+        {
+            movie = new Movie{
+                Id = reader.GetInt32(0),
+                Title = reader.GetString(1),
+                Director = reader.GetString(2),
+                Year = reader.GetInt32(3)
+            };
+        } 
+        return movie;
     }
 
     public void Update(Movie movie)
     {
-        throw new NotImplementedException();
+        using var connection = new SqliteConnection(_connectionString);
+        using var command = connection.CreateCommand();
+        command.CommandText = @"UPDATE Movies SET title = @title, director = @director, year = @year 
+                        WHERE id = @id";
+        command.Parameters.AddWithValue("@title", movie.Title);
+        command.Parameters.AddWithValue("@director", movie.Director);
+        command.Parameters.AddWithValue("@year", movie.Year);
+        command.Parameters.AddWithValue("@id", movie.Id);
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
     }
 }
