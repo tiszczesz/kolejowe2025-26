0328942e855142318c89b41b14fc3f7a8f465962
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-02-16 13:09:13 +0100

    kk

--- 4pr_gr1/node_typescript/cw3/src/cw1.ts
+++ 4pr_gr1/node_typescript/cw3/src/cw1.ts
@@ -1,16 +1,22 @@
 import readline from 'node:readline/promises';
 
 //cin cout z c++
-const rl:readline.Interface = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+const rl: readline.Interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 })
-async function main(){
+async function main() {
     const firstName = await rl.question("Podaj imię: ");
     const age = parseInt(await rl.question("Podaj wiek: "));
+    const correctAge = !isNaN(age) && age > 0;
+    if (!correctAge) {
+        console.log("Niepoprawny wiek");
+        rl.close();
+        return;
+    }
     console.log(firstName);
     console.log(`Witaj ${firstName} 
-        ${(age<18) ? "niepełnoletni": "pełnoletni"}`)
+        ${(age < 18) ? "niepełnoletni" : "pełnoletni"}`)
     rl.close();
 }
 main();
