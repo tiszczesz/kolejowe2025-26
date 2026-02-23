1065dd6429628e757be84442f318bb218565890f
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-02-23 13:23:15 +0100

    kkk

--- 4pr_gr1/node_typescript/cw5_express/src/router.ts
+++ 4pr_gr1/node_typescript/cw5_express/src/router.ts
@@ -1,5 +1,6 @@
 import { Router, type Request,type Response } from "express";
 import fs from 'fs'
+import { users } from "./data/users.js";
 
 //przykładowy router
 // export const router = (req:Request,res:Response)=>{
@@ -13,4 +14,7 @@ router.get("/hello",(req:Request,res:Response)=>{
 router.get("/index",(req:Request,res:Response)=>{
     const html = fs.readFileSync("index.html","utf-8");
     res.send(html)
-})
+});
+router.get("/users",(req:Request,res:Response)=>{
+    res.json(users)
+});
