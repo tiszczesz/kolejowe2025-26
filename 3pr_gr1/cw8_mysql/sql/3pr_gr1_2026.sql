0f48fb655d33358783d817af2d19015646959d6d
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-02-23 19:07:33 +0100

    gg

---
+++ 3pr_gr1/cw8_mysql/sql/3pr_gr1_2026.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 23, 2026 at 07:04 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `3pr_gr1_2026`
+--
+CREATE DATABASE IF NOT EXISTS `3pr_gr1_2026` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+USE `3pr_gr1_2026`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `firstame` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `date_of` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `firstame`, `lastname`, `date_of`) VALUES
+(1, 'Franciszek', 'Małecki', '2026-02-11'),
+(2, 'Danuta', 'Wołek', '2026-02-17'),
+(3, 'Tomasz', 'Góra', '2026-01-11'),
+(4, 'Teresa', 'Meresa', '2024-02-22');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
