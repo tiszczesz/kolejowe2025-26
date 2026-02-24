8e477efcc0f774beee71b49c67a0309bc793f003
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-02-24 09:27:59 +0100

    kk

---
+++ 4pr_gr2/react/cw3-react-bootstrap/src/App.tsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <main className='container'>
+        <h1>Nowa aplikacja React z Bootstrap</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
