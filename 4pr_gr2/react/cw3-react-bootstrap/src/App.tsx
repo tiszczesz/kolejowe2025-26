92957a8120128dc22fe7add6c6425fad96fd5c5f
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-02-24 09:58:44 +0100

    kk

--- 4pr_gr2/react/cw3-react-bootstrap/src/App.tsx
+++ 4pr_gr2/react/cw3-react-bootstrap/src/App.tsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css'
 import Info from './components/Info';
-
+const list = ["jeden", "dwa", "trzy"];
 function App() {
   
 
@@ -10,6 +10,17 @@ function App() {
     <>
       <main className='container'>
         <h1>Nowa aplikacja React z Bootstrap</h1>
+        <ul>
+          {list.map((elem,index)=>(
+            <li key={index}>{elem}</li>
+          ))}
+        </ul>
+        <ol>
+          <li>sadsad</li>
+          <li>www</li>
+          <li>ddd</li>
+          <li>eee</li>
+        </ol>
         <Info />
       </main>
     </>
