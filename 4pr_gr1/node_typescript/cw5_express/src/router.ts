418c4ced69fa2f38afa04262a8d9701bcc2bc4cd
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-02 13:06:46 +0100

    kk

--- 4pr_gr1/node_typescript/cw5_express/src/router.ts
+++ 4pr_gr1/node_typescript/cw5_express/src/router.ts
@@ -1,5 +1,5 @@
-import { Router, type Request,type Response } from "express";
-import { getUserById,createUser,getAllUsers } from "./SqliteRepo.js";
+import { Router, type Request, type Response } from "express";
+import { getUserById, createUser, getAllUsers, deleteUser } from "./SqliteRepo.js";
 import fs from 'fs'
 import { users } from "./data/users.js";
 
@@ -9,36 +9,46 @@ import { users } from "./data/users.js";
 // }
 export const router = Router();
 
-router.get("/hello",(req:Request,res:Response)=>{
+router.get("/hello", (req: Request, res: Response) => {
     res.send("Hello from router")
 })
-router.get("/index",(req:Request,res:Response)=>{
-    const html = fs.readFileSync("index.html","utf-8");
+router.get("/index", (req: Request, res: Response) => {
+    const html = fs.readFileSync("index.html", "utf-8");
     res.send(html)
 });
-router.get("/users",(req:Request,res:Response)=>{
+router.get("/fakeusers", (req: Request, res: Response) => {
     res.json(users)
 });
-router.get("/users/:id",(req:Request,res:Response)=>{
-    const id:number =  parseInt(req.params.id as string); ;
+router.get("/fakeusers/:id", (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id as string);;
     const user = getUserById(id);
-    if(user){
+    if (user) {
         res.json(user)
     } else {
-        res.status(404).json({message:"User not found"})
+        res.status(404).json({ message: "User not found" })
     }
 });
-router.post("/users",(req:Request,res:Response)=>{
-    const {name,email} = req.body;
-    if(name && email){
-        const newUser = createUser(name,email);
+router.post("/users", (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    if (name && email) {
+        const newUser = createUser(name, email);
         res.status(201).json(newUser);
     }
     else {
-        res.status(400).json({message:"Name and email are required"})
+        res.status(400).json({ message: "Name and email are required" })
     }
 })
-router.get("/allusers",(req:Request,res:Response)=>{
+router.get("/users", (req: Request, res: Response) => {
     const allUsers = getAllUsers();
-    res.json(allUsers)  
+    res.json(allUsers)
 });
+router.delete("/users/:id", (req: Request, res: Response) => {
+    //zamieniamy param id z stringa na number, bo w bazie danych id jest typu number
+    const id: number = parseInt(req.params.id as string);
+    const success = deleteUser(id);
+    if (success) {
+        res.json({ message: "User deleted successfully" })
+    } else {
+        res.status(404).json({ message: "User not found" })
+    }
+})
