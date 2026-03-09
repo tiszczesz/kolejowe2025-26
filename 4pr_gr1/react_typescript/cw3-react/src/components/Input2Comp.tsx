780dd75b9f6f79dd5c3915fa12aef4770fa34a53
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-09 14:17:22 +0100

    kk

--- 4pr_gr1/react_typescript/cw3-react/src/components/Input2Comp.tsx
+++ 4pr_gr1/react_typescript/cw3-react/src/components/Input2Comp.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import  { useState,type MouseEvent} from 'react'
 
 type Props = {
     myValue?: string
 }
 
 const Input2Comp = (props: Props) => {
+    const [info, setInfo] = useState('');
+    function handleClick(e: MouseEvent<HTMLInputElement>): void {
+        setInfo(prev => prev + ' ' + e.currentTarget.value);
+    }
+
     return (
         <div>
-            <input type="button" value={props.myValue || "x"} />
+            <input                
+                onClick={(e) => handleClick(e)}
+                type="button" value={props.myValue || "x"} />
             {/* nacisniecie przycisku dodaje value do diva */}
-            <div></div>
+            <div>{info}</div>
         </div>
     )
 }
