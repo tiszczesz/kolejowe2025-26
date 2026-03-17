b78aa7efbc850c6e273e985e06172e7677f03233
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-17 09:23:21 +0100

    kk

---
+++ 4pr_gr2/react/cw6-react-formData/src/App.tsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-center mb-2'>Przegląd filmów</h1>
+      <main className='d-flex gap-2'>
+        <section className='w-50'>Formularz</section>
+        <section className='w-50'>Tabelka</section>
+      </main>
+    </>
+  )
+}
+
+export default App
