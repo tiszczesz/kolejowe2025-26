c1a0517ce0d66a8058bd8c2830caa3efb9559eab
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-17 07:45:45 +0100

    kk

---
+++ 4pr_gr1/react_typescript/cw5-react-table/src/App.tsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main className="d-flex gap-2">
+        <section className="w-50">formuarz</section>
+        <section className="w-50">tabela</section>
+      </main>
+    </>
+  );
+}
+
+export default App;
