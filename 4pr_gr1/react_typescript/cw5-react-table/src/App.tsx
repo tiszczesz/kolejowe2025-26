7e3924af4ab1a28b667a7800cba3b20bdc60ef63
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-17 08:10:46 +0100

    kk

--- 4pr_gr1/react_typescript/cw5-react-table/src/App.tsx
+++ 4pr_gr1/react_typescript/cw5-react-table/src/App.tsx
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { type User,users,getLastUserId,roles } from "./data";
+import { type User, users, getLastUserId, roles } from "./data";
 import { useState } from "react";
 
 function App() {
   const [usersData, setUsersData] = useState<User[]>(users);
+  let lp = 0;
   return (
     <>
       <h1 className="text-center mb-3">Zarządzanie użytkownikami</h1>
@@ -14,9 +15,33 @@ function App() {
         <section className="w-50">
           <table className="table table-striped">
             <thead>
-              <tr>Lp</tr>
-              <tr>Imię i nazwisko</tr>
+              <tr>
+                <th>Lp</th>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>Email</th>
+                <th>Rola</th>
+                <th>Akcje</th>
+              </tr>
             </thead>
+            <tbody>
+              {usersData.map((user) => (
+                <tr
+                  key={user.id}
+                  style={{
+                    textDecoration:
+                      user.role === "admin" ? "underline" : "none",
+                  }}
+                >
+                  <td>{++lp}</td>
+                  <td>{user.firstname}</td>
+                  <td>{user.lastname}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td></td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </section>
       </main>
