29f2a767e43fbaa22306627d4749e899cb0469f2
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-17 09:38:31 +0100

    kk

--- 4pr_gr2/react/cw6-react-formData/src/App.tsx
+++ 4pr_gr2/react/cw6-react-formData/src/App.tsx
@@ -4,6 +4,7 @@ import {type Movie, genres, movies, getLatestMovieId} from './data';
 import { useState } from 'react';
 
 function App() {
+  let lp = 1;
   const [movieList, setMovieList] = useState<Movie[]>(movies);
 
   return (
@@ -11,7 +12,33 @@ function App() {
      <h1 className='text-center mb-2'>Przegląd filmów</h1>
       <main className='d-flex gap-2'>
         <section className='w-50'>Formularz</section>
-        <section className='w-50'>Tabelka</section>
+        <section className='w-50'>
+          <h2>Lista filmów ilość: {movieList.length}</h2>
+          <table className='table table-striped m-2'>
+            <thead>
+              <tr>
+                <th>Lp.</th>
+                <th>Tytuł</th>
+                <th>Reżyser</th>
+                <th>Rok premiery</th>
+                <th>Gatunek</th>
+                <th>Akcje</th>
+              </tr>
+            </thead>
+            <tbody>
+              {movieList.map((movie)=>(
+                <tr key={movie.id}>
+                  <td>{lp++}</td>
+                  <td>{movie.title}</td>
+                  <td>{movie.director}</td>
+                  <td>{movie.releaseYear}</td>
+                  <td>{movie.genre}</td>
+                  <td></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
       </main>
     </>
   )
