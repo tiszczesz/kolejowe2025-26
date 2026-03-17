a7b5de9cfd55f118414a0d1bb6f8d72caab7e549
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-17 07:52:19 +0100

    kk

--- 4pr_gr1/react_typescript/cw5-react-table/src/data.ts
+++ 4pr_gr1/react_typescript/cw5-react-table/src/data.ts
@@ -8,8 +8,8 @@ export const users: User[] = [
     { id: 1, name: "Jan Kowalski", email: "jan.kowalski@example.com", role: "admin" },
     { id: 2, name: "Anna Nowak", email: "anna.nowak@example.com", role: "user" },
     { id: 3, name: "Piotr Wiśniewski", email: "piotr.wisniewski@example.com", role: "guest" },
-  { id: 4, name: "Katarzyna Zielińska", email: "katarzyna.zielinska@example.com", role: "user" },
-  { id: 5, name: "Marek Lewandowski", email: "marek.lewandowski@example.com", role: "guest" },
-  { id: 6, name: "Alicja Kamińska", email: "alicja.kaminska@example.com", role: "admin" },
+    { id: 4, name: "Katarzyna Zielińska", email: "katarzyna.zielinska@example.com", role: "user" },
+    { id: 5, name: "Marek Lewandowski", email: "marek.lewandowski@example.com", role: "guest" },
+    { id: 6, name: "Alicja Kamińska", email: "alicja.kaminska@example.com", role: "admin" },
     
 ];
