d2f07eff2c261e168eb9daa188bc8d216d5dbdf8
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-24 10:44:13 +0100

    kk

--- 4pr_gr2/react/cw7-react-router/src/App.tsx
+++ 4pr_gr2/react/cw7-react-router/src/App.tsx
@@ -1,8 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
+import MyForm from "./pages/MyForm";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "myform", element: <MyForm /> },
+      { path: "contact", element: <Contact /> },
+      { path: "*", element: <PageNotFound /> },
+    ],
+  },
+]);
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- 4pr_gr2/react/cw7-react-router/src/components/Layout.tsx
+++ 4pr_gr2/react/cw7-react-router/src/components/Layout.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { NavLink, Outlet } from "react-router"
 
-type Props = {}
 
-const Layout = (props: Props) => {
+
+const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      <header>
+        <nav className="d-flex gap-2">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/myform">formularz 1</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>&copy; 2026 My App. All rights reserved.</footer>
+    </div>
   )
 }
 
