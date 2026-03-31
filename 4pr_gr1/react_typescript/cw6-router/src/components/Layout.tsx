deadeccde3228263cf9cb4ec15c36bf58f86a6da
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-31 08:35:08 +0200

    kk

--- 4pr_gr1/react_typescript/cw6-router/src/components/Layout.tsx
+++ 4pr_gr1/react_typescript/cw6-router/src/components/Layout.tsx
@@ -1,11 +1,31 @@
+import { Outlet } from "react-router";
 
-
-type Props = {}
+type Props = {};
 
 function Layout({}: Props) {
   return (
-    <div>Layout</div>
-  )
+    <>
+      <header>
+        <nav></nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer
+        style={{
+          padding: "10px",
+          position: "absolute",
+          textAlign: "center",
+          bottom: 0,
+          width: "100%",
+          height: "45px",
+          backgroundColor: "#c4bfbf",
+        }}
+      >
+        &copy; {new Date().getFullYear()} My App. All rights reserved.
+      </footer>
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
