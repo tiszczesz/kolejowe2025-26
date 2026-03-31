b2a223c5d880f5d3ef2bc87cc31f2e61a3b2ac74
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-31 10:43:55 +0200

    kk

--- 4pr_gr2/react/cw7-react-router/src/pages/MyForm.tsx
+++ 4pr_gr2/react/cw7-react-router/src/pages/MyForm.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const MyForm = (props: Props) => {
   return (
-    <div>MyForm</div>
-  )
-}
+    <div>
+      <h2 className="text-center">Formularz z obsługą React_v19</h2>
+      <form action="">
+        <div className="row m-2">
+          <label className="col-3 text-end" htmlFor="firstname">
+            Podaj imię:
+          </label>
+          <input
+            className="col-6"
+            type="text"
+            id="firstname"
+            name="firstname"
+          />
+        </div>
+        <div className="row m-2">
+          <label className="col-3 text-end" htmlFor="lastname">
+            Podaj nazwisko:
+          </label>
+          <input className="col-6" type="text" id="lastname" name="lastname" />
+        </div>
+        <div className="row m-2">
+          <label className="col-3 text-end" htmlFor="age">
+            Podaj wiek:
+          </label>
+          <input className="col-6" type="number" id="age" name="age" />
+        </div>
+        <div className="row m-2">
+          <label className="col-3 text-end" htmlFor="education">
+            Podaj wykształcenie:
+          </label>
+          <select className="col-6" id="education" name="education">
+            <option value="podstawowe">Podstawowe</option>
+            <option value="średnie">Średnie</option>
+            <option value="wyższe">Wyższe</option>
+          </select>
+        </div>
+        <div className="row m-2">
+          <input
+            className="btn btn-outline-primary col-6 offset-3"
+            type="submit"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default MyForm
+export default MyForm;
