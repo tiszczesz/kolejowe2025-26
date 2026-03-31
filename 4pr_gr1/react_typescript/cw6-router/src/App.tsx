59f2dd9832f2e2804d1a4ae6146c60c5260286bc
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-03-31 08:37:19 +0200

    kk

--- 4pr_gr1/react_typescript/cw6-router/src/App.tsx
+++ 4pr_gr1/react_typescript/cw6-router/src/App.tsx
@@ -12,6 +12,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     children: [
+      //elementy do Outlet
       { index: true, element: <Home /> },
       { path: "about", element: <About /> },
       { path: "contact", element: <Contact /> },
