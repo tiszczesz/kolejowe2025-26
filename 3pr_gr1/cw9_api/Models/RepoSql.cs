de505e566f2ec16b1bc2ac6cd20ed3eb089c2562
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-04-13 19:04:10 +0200

    kk

--- 3pr_gr1/cw9_api/Models/RepoSql.cs
+++ 3pr_gr1/cw9_api/Models/RepoSql.cs
@@ -8,7 +8,7 @@ public class RepoSql : IRepoSql
     private readonly string _connectionString;
     public RepoSql(IConfiguration configuration)
     {
-        _connectionString = configuration.GetConnectionString("sqlite") 
+        _connectionString = configuration.GetConnectionString("sqlite")
                 ?? "Data Source=appdb.db";
     }
     public void AddDepartament(Departament departament)
@@ -18,12 +18,28 @@ public class RepoSql : IRepoSql
 
     public void AddStudent(Student student)
     {
-        throw new NotImplementedException();
+        using var connection = new SqliteConnection(_connectionString);
+        using var command = connection.CreateCommand();
+        command.CommandText = @"INSERT INTO Students (Firstname, Lastname, AVG_Grade, Departament_id) 
+                                    VALUES (@firstname, @lastname, @avg_grade, @departament_id)";
+        command.Parameters.AddWithValue("@firstname", student.Firstname);
+        command.Parameters.AddWithValue("@lastname", student.Lastname);
+        command.Parameters.AddWithValue("@avg_grade", student.AVG_Grade);
+        command.Parameters.AddWithValue("@departament_id", student.Departament_id);
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();        
     }
 
     public void DeleteStudent(int id)
     {
-        throw new NotImplementedException();
+        using var connection = new SqliteConnection(_connectionString);
+        using var command = connection.CreateCommand();
+        command.CommandText = @"DELETE FROM Students WHERE Id = @id";
+        command.Parameters.AddWithValue("@id", id);
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close(); 
     }
 
     public List<Departament> GetDepartaments()
@@ -33,18 +49,40 @@ public class RepoSql : IRepoSql
 
     public Student? GetStudent(int id)
     {
-        throw new NotImplementedException();
+        using var connection = new SqliteConnection(_connectionString);
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Students WHERE Id = @id";
+        command.Parameters.AddWithValue("@id", id);
+        connection.Open();
+        var reader = command.ExecuteReader();
+        if (!reader.HasRows)
+        {
+            connection.Close();
+            return null;
+        }
+        reader.Read();
+        var student = new Student
+        {
+            Id = reader.GetInt32(0),
+            Firstname = reader.GetString(1),
+            Lastname = reader.GetString(2),
+            AVG_Grade = reader.GetDouble(3),
+            Departament_id = reader.GetInt32(4)
+        };
+        connection.Close();
+        return student;
     }
 
     public List<Student> GetStudents()
     {
         using var connection = new SqliteConnection(_connectionString);
         using var command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM Student";
+        command.CommandText = "SELECT * FROM Students";
         connection.Open();
-        var reader = command.ExecuteReader();   
+        var reader = command.ExecuteReader();
         var students = new List<Student>();
-        while (reader.Read())        {
+        while (reader.Read())
+        {
             var student = new Student
             {
                 Id = reader.GetInt32(0),
--- 3pr_gr1/cw9_api/Program.cs
+++ 3pr_gr1/cw9_api/Program.cs
@@ -1,8 +1,34 @@
+using cw9_api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+//builder.Services.AddTransient<IRepoSql, RepoSql>();
+builder.Services.AddScoped<IRepoSql, RepoSql>();
+
+//builder.Services.AddSingleton<IRepoSql, RepoSql>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/api/", () => 
      new { Name = "John Doe", Age = 30,Date = DateTime.Now  });
+app.MapGet("/api/students", (IRepoSql repo) => repo.GetStudents());
+app.MapGet("/api/students/{id}", (int id, IRepoSql repo) =>
+{
+     var student = repo.GetStudent(id);
+     if (student == null)
+          return Results.NotFound();
+     return Results.Ok(student);
+});
 
+app.MapPost("/api/students", (Student student, IRepoSql repo) =>
+{
+     repo.AddStudent(student);
+     return Results.Ok();
+});
+app.MapDelete("/api/students/{id}", (int id, IRepoSql repo) =>
+{
+     repo.DeleteStudent(id);
+     return Results.Ok();
+});
 app.Run();
