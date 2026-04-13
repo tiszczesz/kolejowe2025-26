e238e66217b4c5bca0926dc7b84a5b216eb78bb9
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-04-13 18:25:50 +0200

    kk

--- 3pr_gr1/cw9_api/Models/RepoSql.cs
+++ 3pr_gr1/cw9_api/Models/RepoSql.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.Data.Sqlite;
 
 namespace cw9_api.Models;
 
@@ -37,7 +38,25 @@ public class RepoSql : IRepoSql
 
     public List<Student> GetStudents()
     {
-        throw new NotImplementedException();
+        using var connection = new SqliteConnection(_connectionString);
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Student";
+        connection.Open();
+        var reader = command.ExecuteReader();   
+        var students = new List<Student>();
+        while (reader.Read())        {
+            var student = new Student
+            {
+                Id = reader.GetInt32(0),
+                Firstname = reader.GetString(1),
+                Lastname = reader.GetString(2),
+                AVG_Grade = reader.GetDouble(3),
+                Departament_id = reader.GetInt32(4)
+            };
+            students.Add(student);
+        }
+        connection.Close();
+        return students;
     }
 
     public void UpdateStudent(Student student)
