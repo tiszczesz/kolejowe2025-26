69163c9f9f81f7c65a40bb3b9703a12ab539efa9
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-04-20 13:26:34 +0200

    kk

--- 4pr_gr1/react_typescript/cw6-router/src/pages/MyForm1.tsx
+++ 4pr_gr1/react_typescript/cw6-router/src/pages/MyForm1.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import { useState, type SubmitEvent } from "react";
+type FormResult = {
+  name: string;
+  option: string;
+}
 
-type Props = {}
-
-const MyForm1 = (props: Props) => {
+const MyForm1 = () => {
+  console.log('render');
+  const [name, setName] = useState('');
+  const [option, setOption] = useState('option1');
+  const [formResult, setFormResult] = useState<FormResult | null>(null);
+  function handleOnSubmit(e: SubmitEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    console.log(name, option);
+    setFormResult({ name, option });
+  }
   return (
-    <div>Tu będzie formularz 1</div>
+    <>
+      <h2>Formularz 1 z wykorzystaniem onChange -- dużo renderów</h2>
+      <form onSubmit={(e) => handleOnSubmit(e)}>
+        <input onChange={(event) => setName(event.target.value)} type="text" placeholder='podaj imię' value={name} /> <br />
+        <select onChange={((event) => setOption(event.target.value))} value={option}>
+          <option value="option1">Opcja 1</option>
+          <option value="option2">Opcja 2</option>
+          <option value="option3">Opcja 3</option>
+        </select> <br />
+        <input type="submit" value="Wyślij" />
+      </form>
+      <hr />
+      <h3>Wynik:</h3>
+      {formResult && (
+        <pre>{JSON.stringify(formResult, null, 2)}</pre>
+      )}
+    </>
   )
 }
 
