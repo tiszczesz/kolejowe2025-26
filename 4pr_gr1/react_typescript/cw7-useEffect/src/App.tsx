0bb685077bf846f81c66a7310601a8feac76a2aa
Paweł Szczeszek <tiszczesz@users.noreply.github.com>
2026-04-27 13:53:30 +0200

    kk

--- 4pr_gr1/react_typescript/cw7-useEffect/src/App.tsx
+++ 4pr_gr1/react_typescript/cw7-useEffect/src/App.tsx
@@ -1,11 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
+  //głóna częśc komponentu
+  console.log('render')
+  const [info, setInfo] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    console.log('useEffect');
+    if (inputRef.current) {
+      //focusowanie inputa po pierwszym renderze
+      inputRef.current.focus();
+    }
+  }, []);
 
   return (
     <>
       <h1 className='text-center'>Hello, React! - użycie useEffect</h1>
+      <main>
+        <input type="date" name="" id="" /><br />
+        <input ref={inputRef} type="text" value={info}
+          onChange={(e) => setInfo(e.target.value)} />
+        <span>{info}</span>
+      </main>
     </>
   )
 }
